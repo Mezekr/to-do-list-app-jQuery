7c88dd46ac2ebd3a5d7dbc0cd382d8c7ef4bf154
Mezekr W <mezi.edu@tuta.io>
2024-01-10 10:15:48 +0100

    Add a function to create a todo list

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function newItem() {
+	let toDoInput = $("#toDoInput");
+	let toDoInputValue = toDoInput.val();
+	let toDoItem = $("<li></li>");
+
+	// check input value
+	let isInputEmpty = toDoInputValue === "" ? true : false;
+	if (isInputEmpty) {
+		alert("You must write something!");
+	} else {
+		console.log(toDoInputValue);
+		let toToItemList = $("#toDoItemslist");
+		let toDoItemText = toDoItem.text(toDoInputValue);
+		toToItemList.append(toDoItemText);
+		// clear the Input text
+		toDoInput.val("");
+	}
+}
